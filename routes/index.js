254c5878d785cb58d436d56aead5849ab00ea9ec
dpisonero <davidpisonero96@gmail.com>
2018-04-26 15:06:04 +0200

    Completo

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,27 @@
-var express = require('express');
-var router = express.Router();
+let express = require('express');
+let router = express.Router();
+const Sequelize = require('sequelize');
+const sequelize = require("../models/index");
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
   res.render('index', { title: 'Quiz' });
 });
 
-/* Credits page */
+/* GET Credits page */
 router.get('/credits', (req, res, next) => {
   res.render('credits');
+});
+
+/* GET Quizzes page */
+router.get('/quizzes', function(req, res, next) {
+
+    sequelize.models.quiz.findAll().
+    then( quizzes => {
+        res.render('quizzes', {quizzes});
+    })
+        .catch(error => {
+        });
 });
 
 module.exports = router;
