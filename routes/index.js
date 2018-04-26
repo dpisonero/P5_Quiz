768a989555fa3d76e945887b4cfc8f0cca2229e9
dpisonero <davidpisonero96@gmail.com>
2018-04-26 11:06:58 +0200

    Créditos

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', (req, res, next) => {
+  res.render('index', { title: 'Quiz' });
+});
+
+/* Credits page */
+router.get('/credits', (req, res, next) => {
+  res.render('credits');
 });
 
 module.exports = router;
